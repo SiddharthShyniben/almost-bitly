1ddf521c1e76927b3c9b7f827398cb309abcbf59
SiddharthShyniben <siddharth.muscat@gmail.com>
2022-10-27 19:35:55 +0530

    Create redirect component

--- src/app/components/redirect/redirect.component.ts
+++ src/app/components/redirect/redirect.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute,} from '@angular/router';
+import {GenerateService} from 'src/app/services/generate.service';
+
+type DBResult = {
+	  total: number;
+	  documents: Array<{url: string}>;
+}
 
 @Component({
-  selector: 'app-redirect',
-  templateUrl: './redirect.component.html',
-  styleUrls: ['./redirect.component.css']
+	  selector: 'app-redirect',
+	  templateUrl: './redirect.component.html',
+	  styleUrls: ['./redirect.component.css']
 })
 export class RedirectComponent implements OnInit {
+	  failed = false;
 
-  constructor() { }
+	  constructor(private route: ActivatedRoute, private generateService: GenerateService) { }
 
-  ngOnInit(): void {
-  }
-
+	  ngOnInit(): void {
+		  const hash = this.route.snapshot.params['hash'];
+		  this.generateService.getRedirect(hash).subscribe(result => {
+			  if ((result as DBResult).total > 0) {
+				  const link = (result as DBResult).documents[0].url;
+				  window.location.assign(link);
+			  } else {
+				  this.failed = true;
+			  }
+		  })
+	  }
 }
+
